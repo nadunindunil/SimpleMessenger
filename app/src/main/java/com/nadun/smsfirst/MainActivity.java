9eb6b5e0b702f7ba93ee0650ea1de5afa5226cdf
nadunindunil <nadun1indunil@gmail.com>
2015-05-23 00:07:49 +0530

    after error handling

--- app/src/main/java/com/nadun/smsfirst/MainActivity.java
+++ app/src/main/java/com/nadun/smsfirst/MainActivity.java
@@ -1,5 +1,11 @@
 package com.nadun.smsfirst;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
@@ -9,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity  {
@@ -38,10 +45,32 @@ public class MainActivity extends ActionBarActivity  {
     }
 
     protected void sendMsg(String mySms, String myNum) {
+        String SENT = "message sent";
+        String DELIVERED = "message delivered";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0 , new Intent(SENT), 0);
+        PendingIntent deliverPI = PendingIntent.getBroadcast(this, 0 , new Intent(DELIVERED) , 0);
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg1, Intent arg2) {
+                switch (getResultCode()){
+                    case Activity.RESULT_OK:
+                        Toast.makeText(Main.this , "SMS sent" , Toast.LENGTH_LONG).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(),"Generic Failure",Toast.LENGTH_LONG).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(),"No Service",Toast.LENGTH_LONG).show();
+                        break;
+                    
+                }
+            }
+        }, new IntentFilter(SENT));
+
         SmsManager sms = SmsManager.getDefault();
-
-        sms.sendTextMessage(myNum,null,mySms,null,null);
-
+        sms.sendTextMessage(myNum,null,mySms,sentPI,deliverPI);
     }
 
 
